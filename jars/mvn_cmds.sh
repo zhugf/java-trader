e53a9e45f83dea6a5fa8855d9377f408cca2c1e2
zhugf <zhugf000@gmail.com>
2020-12-30 00:52:08 +0800

    增加JCTP 6.3.19 本地maven安装命令和jar文件
    并删除过期的6.3.11/13/15相关jar文件

--- jars/mvn_cmds.sh
+++ jars/mvn_cmds.sh
@@ -2,59 +2,57 @@ mvn install:install-file \
     -Dfile=jtrader-common-debug-20190528.jar \
     -DgroupId=net.jtrader -DartifactId=jtrader-common -Dversion=1.0.0.0-20190528 -Dpackaging=jar
 
-#JCTP 6.3.11
+
+#JCTP 6.3.19-20200423
 mvn install:install-file \
-	-Dfile=jctp-win32_x86-6.3.11_20180109-debug.jar \
-	-DgroupId=net.jtrader -DartifactId=jctp -Dversion=6.3.11-20180109-win32_x86 -Dpackaging=jar
+    -Dfile=jctp-linux_x64-6.3.19_20200106.jar \
+    -Djavadoc=jctp-linux_x64-6.3.19_20200106-docs.jar \
+    -DgroupId=net.jtrader -DartifactId=jctp -Dversion=6.3.19_20200106-linux_x64 -Dpackaging=jar
 
 mvn install:install-file \
-	-Dfile=jctp-win32_x64-6.3.11_20180109-debug.jar \
-	-DgroupId=net.jtrader -DartifactId=jctp -Dversion=6.3.11-20180109-win32_x64 -Dpackaging=jar
+    -Dfile=jctp-win32_x64-6.3.19_20200106.jar \
+    -Djavadoc=jctp-win32_x64-6.3.19_20200106-docs.jar \
+    -DgroupId=net.jtrader -DartifactId=jctp -Dversion=6.3.19_20200106-win32_x64 -Dpackaging=jar
 
 mvn install:install-file \
-	-Dfile=jctp-linux_x64-6.3.11_20180109-debug.jar \
-	-DgroupId=net.jtrader -DartifactId=jctp -Dversion=6.3.11-20180109-linux_x64 -Dpackaging=jar
+    -Dfile=jctp-win32_x86-6.3.19_20200106.jar \
+    -Djavadoc=jctp-win32_x86-6.3.19_20200106-docs.jar \
+    -DgroupId=net.jtrader -DartifactId=jctp -Dversion=6.3.19_20200106-win32_x86 -Dpackaging=jar
 
-#JCTP 6.3.13
+#JCTP 6.3.19-20200423
 mvn install:install-file \
-    -Dfile=jctp-linux_x64-6.3.13_20181119-debug.jar \
-    -DgroupId=net.jtrader -DartifactId=jctp -Dversion=6.3.13_20181119-linux_x64 -Dpackaging=jar
-
-mvn install:install-file \
-    -Dfile=jctp-win32_x64-6.3.13_20181119-debug.jar \
-    -DgroupId=net.jtrader -DartifactId=jctp -Dversion=6.3.13_20181119-win32_x64 -Dpackaging=jar
+    -Dfile=jctp-linux_x64-6.3.19_20200423.jar \
+    -Djavadoc=jctp-linux_x64-6.3.19_20200423-docs.jar \
+    -DgroupId=net.jtrader -DartifactId=jctp -Dversion=6.3.19_20200423-linux_x64 -Dpackaging=jar
 
 mvn install:install-file \
-    -Dfile=jctp-win32_x86-6.3.13_20181119-debug.jar \
-    -DgroupId=net.jtrader -DartifactId=jctp -Dversion=6.3.13_20181119-win32_x86 -Dpackaging=jar
-
-#JCTP 6.3.15
-mvn install:install-file \
-    -Dfile=jctp-linux_x64-6.3.15_20190220-debug.jar \
-    -DgroupId=net.jtrader -DartifactId=jctp -Dversion=6.3.15_20190220-linux_x64 -Dpackaging=jar
+    -Dfile=jctp-win32_x64-6.3.19_20200423.jar \
+    -Djavadoc=jctp-win32_x64-6.3.19_20200423-docs.jar \
+    -DgroupId=net.jtrader -DartifactId=jctp -Dversion=6.3.19_20200423-win32_x64 -Dpackaging=jar
 
 mvn install:install-file \
-    -Dfile=jctp-win32_x64-6.3.15_20190220-debug.jar \
-    -DgroupId=net.jtrader -DartifactId=jctp -Dversion=6.3.15_20190220-win32_x64 -Dpackaging=jar
-
-mvn install:install-file \
-    -Dfile=jctp-win32_x86-6.3.15_20190220-debug.jar \
-    -DgroupId=net.jtrader -DartifactId=jctp -Dversion=6.3.15_20190220-win32_x86 -Dpackaging=jar
+    -Dfile=jctp-win32_x86-6.3.19_20200423.jar \
+    -Djavadoc=jctp-win32_x86-6.3.19_20200423-docs.jar \
+    -DgroupId=net.jtrader -DartifactId=jctp -Dversion=6.3.19_20200423-win32_x86 -Dpackaging=jar
 
 #JCTP 6.5.1-20200904
 mvn install:install-file \
     -Dfile=jctp-linux_x64-6.5.1_20200904.jar \
+    -Djavadoc=jctp-linux_x64-6.5.1_20200904-docs.jar \
     -DgroupId=net.jtrader -DartifactId=jctp -Dversion=6.5.1_20200904-linux_x64 -Dpackaging=jar
 
 mvn install:install-file \
     -Dfile=jctp-win32_x64-6.5.1_20200904.jar \
+    -Djavadoc=jctp-win32_x64-6.5.1_20200904-docs.jar \
     -DgroupId=net.jtrader -DartifactId=jctp -Dversion=6.5.1_20200904-win32_x64 -Dpackaging=jar
 
 #JCTP 6.5.1-20200908
 mvn install:install-file \
     -Dfile=jctp-linux_x64-6.5.1_20200908.jar \
+    -Djavadoc=jctp-linux_x64-6.5.1_20200908-docs.jar \
     -DgroupId=net.jtrader -DartifactId=jctp -Dversion=6.5.1_20200908-linux_x64 -Dpackaging=jar
 
 mvn install:install-file \
     -Dfile=jctp-win32_x64-6.5.1_20200908.jar \
+    -Djavadoc=jctp-win32_x64-6.5.1_20200908-docs.jar \
     -DgroupId=net.jtrader -DartifactId=jctp -Dversion=6.5.1_20200908-win32_x64 -Dpackaging=jar
