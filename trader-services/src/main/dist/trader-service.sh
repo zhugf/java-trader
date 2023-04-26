aec1254d9b52f7b54e9fcca4c88fc8af02e0c850
zhugf <zhugf000@gmail.com>
2023-04-26 21:37:23 +0800

    trader-service.sh check unexpected exit

--- trader-services/src/main/dist/trader-service.sh
+++ trader-services/src/main/dist/trader-service.sh
@@ -2,4 +2,19 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
 
-./trader.sh service start --tradingTimesOnly=true >> $TRADER_HOME/logs/trader.out 2>&1 &
+i=0
+while [[ $i -lt 3 ]]
+do
+    ((i++))
+    #echo "第 $i 次"
+    ./trader.sh service start --tradingTimesOnly=true >> $TRADER_HOME/logs/trader.out 2>&1 &
+    sleep 60
+    TRADER_EXISTS=`pgrep java -a|grep trader|wc -l`
+    if [ $TRADER_EXISTS=="0" ]; 
+    then
+        #启动失败尝试重启
+        continue
+    else
+        break
+    fi
+done
